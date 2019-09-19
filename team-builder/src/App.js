dfe5d7c869a52401a7906f16d56d43562d116437
Mary <mary.clayton1@hotmail.com>
2019-09-19 16:41:36 -0700

    met mvp 2

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import TeamList from "./components/TeamList"
+
+import TeamMembers from "./components/TeamMember.js";
 import TeamNav from "./components/TeamNav"
+import Form from "./components/Form.js";
+import users from "./components/data"
 
 function App() {
+
+
+  const [teamMembers, setTeamMembers] = useState (users)
+
+  const addNewTeamMember = team => {
+    const newTeamMember = {
+      name: team.name,
+      email: team.email,
+      role: team.role
+    };
+    setTeamMembers([...teamMembers, newTeamMember]);
+  };
+
+
+
   return (
     <div className="App">
       <TeamNav />
-      <TeamList />
+      <TeamMembers teamMembers = {teamMembers} />
+      <Form addNewTeamMember = {addNewTeamMember}/>
     </div>
   );
 }
